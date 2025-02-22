a142eddc7a67f3c264c61326c17146fe142e54da
renelopezjikko <123493219+renelopezjikko@users.noreply.github.com>
2025-02-22 15:08:26 -0300

    Quartz sync: Feb 22, 2025, 3:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -67,7 +67,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false, highlight: true, }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false, }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
